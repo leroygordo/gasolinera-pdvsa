abe8b32e05df005a99d65ead65c37822ed89d275
Hancel Gonzalez <hancel@localhost.localdomain>
2013-01-22 20:46:32 -0430

    validacion y lectura argumentos en bomba.c

--- bomba.c
+++ bomba.c
@@ -2,76 +2,56 @@
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <getopt.h>
 
 # define TRUE 1
 # define FALSE 0
 
 static void print_use(){
-  printf("uso: centro OPCIONES ...\n");
+  printf("uso: bomba OPCIONES ...\n");
   printf("     OPCIONES:\n");
-  printf("       -n  <nombre_centro>\n");
+  printf("       -n  <nombre_bomba>\n");
   printf("       -cp <capacidad_maxima>\n");
   printf("       -i  <inventario>\n");
-  printf("       -t  <tiempo>\n");
-  printf("       -s  <suministro>\n");
-  printf("       -p  <puerto>\n");
+  printf("       -c  <consumo>\n");
+  printf("       -fc  <fichero_centros>\n");
 }
 
 int main(int argc, char **argv) {
 
   // Validacion y lectura de argumentos
 
-  if(argc != 13){
+  if(argc != 11){
     print_use();
     exit(EXIT_FAILURE);
   }
+ 
+  char *nombre_bomba, *fichero_centros;
+  int capacidad, inventario, consumo;
 
-  char * nombre_centro;
-  int capacidad, inventario, tiempo, suministro, puerto;
-
-  // getopt no parsea argumentos cuyo flag es mas de un caracter
-  int cp = FALSE;
   {
    int j;
-   for(j = 0 ; j < 6 ; j++)
-     if(!strcmp(argv[2*j + 1],"-cp")) {
-       cp = TRUE;
+   for(j = 0 ; j < 5 ; j++) {
+     if(!strcmp(argv[2 * j + 1],"-cp")) {
        capacidad = atoi(argv[2*j+2]);
-       break;
      }
+     else if(!strcmp(argv[2 * j + 1],"-fc")) {
+       fichero_centros = argv[2 * j + 2];
+     }
+     else if(!strcmp(argv[2 * j + 1],"-n")) {
+       nombre_bomba = argv[2 * j+2];
+     }
+     else if(!strcmp(argv[2 * j + 1],"-i")) {
+       inventario = atoi(argv[2 * j+2]);
+     }
+     else if(!strcmp(argv[2 * j + 1],"-c")) {
+       consumo = atoi(argv[2 * j + 2]);
+     }
+     else {
+        print_use();
+        exit(EXIT_FAILURE);
+     }
+   }
   }
-  
-  if(!cp)
-    printf("uso:\n");
-  else 
-    while(TRUE){
-      int options = getopt(argc,argv,"n:i:t:s:p:c:");
-      if (options == -1)
-        break;
-      switch(options) {
-        case 'n':
-          nombre_centro = optarg;
-          break;      
-        case 'i':
-          inventario = atoi(optarg);
-          break;
-        case 't':
-          tiempo = atoi(optarg);
-          break;
-        case 's':
-          suministro = atoi(optarg);
-          break;
-        case 'p':
-          puerto = atoi(optarg);
-          break;
-        case 'c':
-          break;
-        default:
-          print_use();
-          break;
-      }
-    }
 
-  printf("%s %d %d %d %d %d\n",nombre_centro,capacidad,inventario,tiempo,suministro,puerto);
+  printf("%s %d %d %d %s\n",nombre_bomba,capacidad,inventario,consumo,fichero_centros);
 }
