b032dcbc362c1aa2d50501b69f034a5d89a2cdec
Hancel Gonzalez <hancel.gonzalez@gmail.com>
2013-02-09 00:36:19 -0430

    servirdor.sh en ldc

--- servidor.sh
+++ servidor.sh
@@ -1,7 +1,7 @@
-./centro -n centro -cp 40 -i 45 -t 10 -s 8 -p 10221 &
-./centro -n centro -cp 40 -i 45 -t 3 -s 9 -p 10222 &
-./centro -n centro -cp 40 -i 45 -t 1 -s 9 -p 10223 &
-./centro -n centro -cp 40 -i 45 -t 2 -s 8 -p 10224 &
-./centro -n centro -cp 40 -i 45 -t 1 -s 8 -p 10225 &
-./centro -n centro -cp 40 -i 45 -t 100 -s 8 -p 10226 &
-./centro -n centro -cp 40 -i 45 -t 5 -s 8 -p 10227 &
+./centro -n centro -cp 40000 -i 45 -t 10 -s 8 -p 10221 &
+./centro -n centro -cp 40000 -i 45 -t 3 -s 9 -p 10222 &
+./centro -n centro -cp 40000 -i 45 -t 1 -s 9 -p 10223 &
+./centro -n centro -cp 40000 -i 45 -t 2 -s 8 -p 10224 &
+./centro -n centro -cp 40000 -i 45 -t 1 -s 8 -p 10225 &
+./centro -n centro -cp 40000 -i 45 -t 100 -s 8 -p 10226 &
+./centro -n centro -cp 40000 -i 45 -t 5 -s 8 -p 10227 &
